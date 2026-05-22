c0ad00926062b4adf150989c85cd69d7a4876ea0
HP <HP@gmail.com>
2026-05-22 14:51:56 +0530

    chore: initial scratch deployment with secure variables

--- main.tf
+++ main.tf
@@ -13,11 +13,13 @@ provider "azurerm" {
   features {}
 }
 
+# Creates Resource Group in India
 resource "azurerm_resource_group" "rg" {
   name     = "rg-pipeline-demo"
-  location = "centralindia" 
+  location = "centralindia"
 }
 
+# Maps parameters cleanly to the child module above
 module "my_vm" {
   source              = "./modules/virtual_machine"
   resource_group_name = azurerm_resource_group.rg.name
--- modules/virtual_machine/main.tf
+++ modules/virtual_machine/main.tf
@@ -1,10 +1,44 @@
-# Inside modules/virtual_machine/main.tf
+# modules/virtual_machine/main.tf
 
+# 1. EXPLICIT INPUT VARIABLES (This fixes the "Unsupported argument" error)
+variable "resource_group_name" { type = string }
+variable "location"            { type = string }
+
+# 2. VIRTUAL NETWORK
+resource "azurerm_virtual_network" "vnet" {
+  name                = "demo-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = var.resource_group_name
+}
+
+# 3. SUBNET
+resource "azurerm_subnet" "subnet" {
+  name                 = "demo-subnet"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# 4. NETWORK INTERFACE
+resource "azurerm_network_interface" "nic" {
+  name                = "demo-nic"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# 5. VIRTUAL MACHINE (Free Tier Eligible in India)
 resource "azurerm_linux_virtual_machine" "vm" {
   name                = "demo-vm"
   resource_group_name = var.resource_group_name
   location            = var.location
-  size                = "Standard_B2ats_v2" # <-- Updated to Free Tier AMD size
+  size                = "Standard_B2ats_v2" # Free tier eligible compute size
   admin_username      = "azureuser"
   
   network_interface_ids = [azurerm_network_interface.nic.id]
@@ -14,7 +48,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   os_disk {
     caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS" # Standard HDD/SSD is covered under free allocation
+    storage_account_type = "Standard_LRS"
   }
 
   source_image_reference {
