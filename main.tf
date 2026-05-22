ccea64ffeb9320dee85a4f05b4e084398dce6932
HP <HP@gmail.com>
2026-05-22 14:35:53 +0530

    chore: initial scratch deployment with secure variables

--- main.tf
+++ main.tf
@@ -11,32 +11,13 @@ provider "azurerm" {
   features {}
 }
 
-# Create a Resource Group
 resource "azurerm_resource_group" "rg" {
-  name     = "default-rg"
+  name     = "rg-pipeline-demo"
   location = "East US"
 }
 
-# Create a Virtual Network
-resource "azurerm_virtual_network" "vnet" {
-  name                = "default-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
+module "my_vm" {
+  source              = "./modules/virtual_machine"
   resource_group_name = azurerm_resource_group.rg.name
-}
-
-# Create a Subnet
-resource "azurerm_subnet" "subnet" {
-  name                 = "default-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-# Call the Child Module to build the VM
-module "my_basic_vm" {
-  source    = "./modules/basic_vm"
-  rg_name   = azurerm_resource_group.rg.name
-  location  = azurerm_resource_group.rg.location
-  subnet_id = azurerm_subnet.subnet.id
+  location            = azurerm_resource_group.rg.location
 }
---
+++ modules/virtual_machine/main.tf
@@ -0,0 +1,53 @@
+variable "resource_group_name" {}
+variable "location"            {}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "demo-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "demo-subnet"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "demo-nic"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "demo-vm"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  size                = "Standard_B1s" # Free Tier Tier Eligible
+  admin_username      = "azureuser"
+  
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  admin_password                  = "SecurePassword123!"
+  disable_password_authentication = false
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
